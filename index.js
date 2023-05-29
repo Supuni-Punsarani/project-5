c99ae2f72f217cb1994ff647f1d7c4d749be2bda
Supuni-Punsarani <114279717+Supuni-Punsarani@users.noreply.github.com>
2023-05-29 19:47:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function area(){
+    //alert("Button test");
+    let b=parseFloat(document.getElementById("leg1").value);
+    let h=parseFloat(document.getElementById("leg2").value);
+    document.getElementById("result").value=0.5*b*h;
+  
+}
